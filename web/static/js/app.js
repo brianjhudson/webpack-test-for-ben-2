3fdb1afa1ba4204e9ea101e10d6b5819af50709e
Brian Hudson <brianjasonhudson@gmail.com>
2017-05-28 16:48:07 -0500

    Finished setup

---
+++ web/static/js/app.js
@@ -0,0 +1,7 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+document.addEventListener("DOMContentLoaded", () => {
+    const reactNode = document.getElementById("react-node")
+    ReactDOM.render(<h1>Hello</h1>, reactNode)
+})
